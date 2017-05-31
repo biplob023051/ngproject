1d4f86ab1245205f0602d3d21ce4c60741e52712
Biplob <biplob.weblancer@gmail.com>
2017-06-01 01:52:57 +0600

    Reactive form approach with custom validation method and custom pipe

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { CapitalizePipe } from '../shared/capitalize.pipe';
 
 @NgModule({
 	declarations: [
 		SignupComponent,
-    	SigninComponent
+    	SigninComponent,
+    	CapitalizePipe
 	],
 	imports: [
 		FormsModule,
+		ReactiveFormsModule,
 		CommonModule,
 		AuthRoutingModule,
 	]
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+	genders = ['male', 'female'];
+	signUpForm: FormGroup;
+	forbiddenUserNames = ['Admin', 'Owner'];
   constructor() { }
-
   ngOnInit() {
+  	this.signUpForm = new FormGroup({
+  		'name': new FormGroup({
+  			'fname': new FormControl(null, [Validators.required, this.forbiddenName.bind(this)]),
+  			'lname': new FormControl(null, [Validators.required, this.forbiddenName.bind(this)])
+  		}),
+  		'email': new FormControl(null, [Validators.required, Validators.email]),
+  		'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+  		'gender': new FormControl('male'),
+  		'hobbies': new FormArray([
+  			new FormControl(null, Validators.required),
+  			new FormControl(null, Validators.required)
+  		])
+  	});
+  }
+
+  addMoreHobbies() {
+  	let control = new FormControl(null, Validators.required);
+  	(<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  forbiddenName(control: FormControl) : {[s:string] : boolean} {
+  	if (this.forbiddenUserNames.indexOf(control.value) != -1) {
+  		return {'forbiddenNames': true};
+  	}
+  }
+
+  onSignup() {
+  	console.log(this.signUpForm);
   }
 
 }
